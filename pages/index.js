fbd7a1761437f866858950b850abca44ae53e63d
nalindadsn <nalindadsn@gmail.com>
2021-12-18 20:02:50 +0530

    navbar ad 3

--- pages/index.js
+++ pages/index.js
@@ -98,52 +98,52 @@ export default function Home() {
   </div>
 </nav>
 <div className='bg-gray-800'>oop</div>
-<div class="container m-auto p-8 text-grey-darkest ">
+<div className="container m-auto p-8 text-grey-darkest ">
 
-   <h1 class="mb-8">Tailwind CSS flex grid</h1>
+   <h1 className="mb-8">Tailwind CSS flex grid</h1>
 
-   <h2 class="mb-4">Column Spacing</h2>
-   <p class="mb-4">Example of column spacing from the <a href="https://tailwindcss.com/docs/examples/grids/#column-spacing" class="text-indigo">documentation.</a></p>
+   <h2 className="mb-4">Column Spacing</h2>
+   <p className="mb-4">Example of column spacing from the <a href="https://tailwindcss.com/docs/examples/grids/#column-spacing" className="text-indigo">documentation.</a></p>
 
-   <div class="flex -mx-2 mb-8">
-      <div class="w-1/3 px-2">
-         <div class="bg-grey-light h-12"></div>
+   <div className="flex -mx-2 mb-8">
+      <div className="w-1/3 px-2">
+         <div className="bg-grey-light h-12"></div>
       </div>
-      <div class="w-1/3 px-2">
-         <div class="bg-grey h-12"></div>
+      <div className="w-1/3 px-2">
+         <div className="bg-grey h-12"></div>
       </div>
-      <div class="w-1/3 px-2">
-         <div class="bg-grey-light h-12"></div>
+      <div className="w-1/3 px-2">
+         <div className="bg-grey-light h-12"></div>
       </div>
    </div>
 
-   <h2 class="mb-4">Responsive 3 col flex grid</h2>
+   <h2 className="mb-4">Responsive 3 col flex grid</h2>
 
-   <div class="flex flex-wrap -mx-2 mb-8">
-      <div class="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
-         <div class="border h-12 text-sm text-grey-dark flex items-center justify-center">
+   <div className="flex flex-wrap -mx-2 mb-8">
+      <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+         <div className="border h-12 text-sm text-grey-dark flex items-center justify-center">
             <p>full / half / quarter</p>
          </div>
       </div>
-      <div class="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
-         <div class="border h-12 text-sm text-grey-dark flex items-center justify-center">
+      <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+         <div className="border h-12 text-sm text-grey-dark flex items-center justify-center">
             <p>full / half / quarter</p>
          </div>
       </div>
-      <div class="w-full lg:w-1/2 px-2">
-         <div class="border h-12 text-sm text-grey-dark flex items-center justify-center">
+      <div className="w-full lg:w-1/2 px-2">
+         <div className="border h-12 text-sm text-grey-dark flex items-center justify-center">
             <p>full / half</p>
          </div>
       </div>
    </div>
 
-   <h2 class="mb-4">Using borders for gutters</h2>
+   <h2 className="mb-4">Using borders for gutters</h2>
 <div className='bg-gray-800'>
 
-   <div class="mb-8 p-2 w-full flex flex-wrap bg-grey-light ">
-      <div class="h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey"></div>
-      <div class="h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey"></div>
-      <div class="h-16 border-8 w-full lg:w-1/2 bg-grey"></div>
+   <div className="mb-8 p-2 w-full flex flex-wrap bg-grey-light ">
+      <div className="h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey"></div>
+      <div className="h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey"></div>
+      <div className="h-16 border-8 w-full lg:w-1/2 bg-grey"></div>
    </div>
 </div>
 
