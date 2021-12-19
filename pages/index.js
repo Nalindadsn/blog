3caf8f2abf9cdc941f0404e99054b89a64fdb9fa
nalindadsn <nalindadsn@gmail.com>
2021-12-20 00:10:36 +0530

    add 21

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,14 @@ import React, { useState } from "react";
 export default function Home() {
 
   const [showMe, setShowMe] = useState(false);
+  const [showMe2, setShowMe2] = useState(false);
+
   function toggle(){
     setShowMe(!showMe);
+  }
+
+  function toggleuser(){
+    setShowMe2(!showMe2);
   }
 
   return (
@@ -22,7 +28,7 @@ export default function Home() {
     
 
 
-      <nav className="bg-gray-800 w-full h-16  px-8   shadow-md fixed" >
+      <nav className="bg-gray-800 w-full h-16  px-8   shadow-md fixed z-50" >
   <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
     <div className="relative flex items-center justify-between h-16">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -41,9 +47,9 @@ export default function Home() {
         </button>
       </div>
       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex-shrink-0 flex items-center">
-          <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-          <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+        <div className="flex-shrink-0 flex items-center text-white">
+          CODEADDON
+
         </div>
         <div className="hidden sm:block sm:ml-6">
           <div className="flex space-x-4">
@@ -72,17 +78,19 @@ export default function Home() {
           <div>
             <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
               <span className="sr-only">Open user menu</span>
-              <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <img  onClick={toggleuser}  className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
             </button>
           </div>
 
 
-          {/* <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+          <div  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1"  style={{
+        display: showMe2?"block":"none"
+      }}>
 
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
@@ -379,75 +387,75 @@ export default function Home() {
 
       <footer className={styles.footer}>
 
-{/* 
+
       <div className="footer-2 bg-gray-800 pt-6 md:pt-12">
-  <div className="container px-4 mx-auto">
+          <div className="container px-4 mx-auto">
 
-    <div className="md:flex md:flex-wrap md:-mx-4 py-6 md:pb-12">
+            <div className="md:flex md:flex-wrap md:-mx-4 py-6 md:pb-12">
 
-      <div className="footer-info lg:w-1/3 md:px-4">
-        <h4 className="text-white text-2xl mb-4">19K users are using FWR blocks and making their life easy.</h4>
-        <p className="text-gray-400">We have carefully crafted the blocks to suit to everyone's need.</p>
-        <div className="mt-4">
-          <button className="bg-facebook py-2 px-4 text-white rounded mt-2 transition-colors duration-300">
-            <span className="fab fa-facebook-f mr-2"></span> Follow
-          </button>
-          <button className="bg-twitter py-2 px-4 text-white rounded ml-2 mt-2 transition-colors duration-300">
-            <span className="fab fa-twitter mr-2"></span> Follow @freeweb19
-          </button>
-        </div>
-      </div>
+              <div className="footer-info lg:w-1/3 md:px-4">
+                <h4 className="text-white text-2xl mb-4">19K users are using FWR blocks and making their life easy.</h4>
+                <p className="text-gray-400">We have carefully crafted the blocks to suit to everyone's need.</p>
+                <div className="mt-4">
+                  <button className="bg-facebook py-2 px-4 text-white rounded mt-2 transition-colors duration-300">
+                    <span className="fab fa-facebook-f mr-2"></span> Follow
+                  </button>
+                  <button className="bg-twitter py-2 px-4 text-white rounded ml-2 mt-2 transition-colors duration-300">
+                    <span className="fab fa-twitter mr-2"></span> Follow @freeweb19
+                  </button>
+                </div>
+              </div>
 
-      <div className="md:w-2/3 lg:w-1/3 md:px-4 xl:pl-16 mt-12 lg:mt-0">
-        <div className="sm:flex">
-          <div className="sm:flex-1">
-            <h6 className="text-base font-medium text-white uppercase mb-2">About</h6>
-            <div>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Company</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Culture</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Team</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Careers</a>
+              <div className="md:w-2/3 lg:w-1/3 md:px-4 xl:pl-16 mt-12 lg:mt-0">
+                <div className="sm:flex">
+                  <div className="sm:flex-1">
+                    <h6 className="text-base font-medium text-white uppercase mb-2">About</h6>
+                    <div>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Company</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Culture</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Team</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Careers</a>
+                    </div>
+                  </div>
+                  <div className="sm:flex-1 mt-4 sm:mt-0">
+                    <h6 className="text-base font-medium text-white uppercase mb-2">What we offer</h6>
+                    <div>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Blocks</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Resources</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Tools</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Tutorials</a>
+                      <a href="#" className="text-gray-400 py-1 block hover:underline">Freebies</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="md:w-1/3 md:px-4 md:text-center mt-12 lg:mt-0">
+                <h5 className="text-lg text-white font-medium mb-4">Explore our site</h5>
+                <button className="bg-indigo-600 text-white hover:bg-indigo-700 rounded py-2 px-6 md:px-12 transition-colors duration-300">Explore</button>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="border-t border-solid border-gray-900 mt-4 py-4">
+            <div className="container px-4 mx-auto">
+
+              <div className="md:flex md:-mx-4 md:items-center">
+                <div className="md:flex-1 md:px-4 text-center md:text-left">
+                  <p className="text-white">&copy; <strong>CODEADDON</strong></p>
+                </div>
+                <div className="md:flex-1 md:px-4 text-center md:text-right">
+                  <a href="#" className="py-2 px-4 text-white inline-block hover:underline">Terms of Service</a>
+                  <a href="#" className="py-2 px-4 text-white inline-block hover:underline">Privacy Policy</a>
+                </div>
+              </div>
+
             </div>
           </div>
-          <div className="sm:flex-1 mt-4 sm:mt-0">
-            <h6 className="text-base font-medium text-white uppercase mb-2">What we offer</h6>
-            <div>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Blocks</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Resources</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Tools</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Tutorials</a>
-              <a href="#" className="text-gray-400 py-1 block hover:underline">Freebies</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="md:w-1/3 md:px-4 md:text-center mt-12 lg:mt-0">
-        <h5 className="text-lg text-white font-medium mb-4">Explore our site</h5>
-        <button className="bg-indigo-600 text-white hover:bg-indigo-700 rounded py-2 px-6 md:px-12 transition-colors duration-300">Explore</button>
-      </div>
 
     </div>
-
-  </div>
-
-  <div className="border-t border-solid border-gray-900 mt-4 py-4">
-    <div className="container px-4 mx-auto">
-
-      <div className="md:flex md:-mx-4 md:items-center">
-        <div className="md:flex-1 md:px-4 text-center md:text-left">
-          <p className="text-white">&copy; <strong>CODEADDON</strong></p>
-        </div>
-        <div className="md:flex-1 md:px-4 text-center md:text-right">
-          <a href="#" className="py-2 px-4 text-white inline-block hover:underline">Terms of Service</a>
-          <a href="#" className="py-2 px-4 text-white inline-block hover:underline">Privacy Policy</a>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-</div> */}
 
       </footer>
     </div>
