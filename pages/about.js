7c27941d803296c8412f21494cc08659505a6540
nalindadsn <nalindadsn@gmail.com>
2021-12-20 22:03:42 +0530

    add 32

--- pages/about.js
+++ pages/about.js
@@ -54,14 +54,14 @@ export default function About() {
         <div className="hidden sm:block sm:ml-6">
           <div className="flex space-x-4">
 
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium" aria-current="page">Home</a>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium rounded-md " aria-current="page">Home</a>
 
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium">Articles</a>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium rounded-md ">Articles</a>
 
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium">Video Tutorials</a>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium rounded-md ">Video Tutorials</a>
 
-            <a href="#" className="bg-gray-900 text-white px-3 py-2  text-sm font-medium">About Us</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium">Contact Us</a>
+            <a href="#" className="bg-gray-900 text-white px-3 py-2  text-sm font-medium rounded-md ">About Us</a>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2  text-sm font-medium rounded-md ">Contact Us</a>
           </div>
         </div>
       </div>
