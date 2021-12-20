f9cd218a7c87836f352b1549dd3d7e4d5d158801
nalindadsn <nalindadsn@gmail.com>
2021-12-20 12:47:34 +0530

    add 27

--- pages/playlists.js
+++ pages/playlists.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 export default function Playlist() {
 
-  const [showMe, setShowMe] = useState(false);
+  const [showMe, setShowMe] = useState(true);
   const [showMe2, setShowMe2] = useState(false);
 
   function toggle(){
@@ -40,18 +40,6 @@ export default function Playlist() {
     <div className="relative flex items-center justify-between h-16">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 
-        <button  onClick={toggle} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          
-          
-          <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-          
-          <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
       </div>
       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div className="flex-shrink-0 flex items-center text-white">
@@ -91,7 +79,7 @@ export default function Playlist() {
           </div>
 
 
-          <div  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1"  style={{
+          <div  className="z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1"  style={{
         display: showMe2?"block":"none"
       }}>
 
@@ -195,16 +183,28 @@ export default function Playlist() {
         </nav>
     </aside>
 
-    <main id="content" className="flex-1 p-6 lg:px-8">
+    <main id="content" className="flex-1 lg:px-8">
         <div className="max-w-7xl mx-auto">
 
-            <div className="px-4 py-6 sm:px-0">
+            <div className="px-4  sm:px-0">
                 <div className=" rounded-lg h-96">
 
 
 {/* 
 ============================================== */}
 
+<button style={{marginLeft:"-40px"}}  onClick={toggle} type="button" className="absalute inline-flex items-center justify-center p-2 text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          
+          
+          <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          
+          <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+</button>
 
 <div className="flex ">
   <div className="w-1/2 h-12"><h1 className="font-bold text-3xl ">Playlists</h1></div>
