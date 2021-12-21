6c630946f8b4758245532fc234eb17c45b0c956e
nalindadsn <nalindadsn@gmail.com>
2021-12-21 19:56:06 +0530

    add 36

--- pages/playlists.js
+++ pages/playlists.js
@@ -6,7 +6,7 @@ export default function Playlists() {
 
 
   
-  const [showMe, setShowMe] = useState(true);
+  const [showMe, setShowMe] = useState('');
 
   
   const [showMe2, setShowMe2] = useState(false);
@@ -111,7 +111,7 @@ export default function Playlists() {
 
 
 
-    <aside id="sidebar" className="z-50 bg-gray-800 text-gray-100 md:w-64 w-3/4 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto" data-dev-hint="sidebar; px-0 for frameless; px-2 for visually inset the navigation"  style={{
+    <aside id="sidebar" className="z-50 bg-gray-800 text-gray-100 md:w-64 w-3/4 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto block  " data-dev-hint="sidebar; px-0 for frameless; px-2 for visually inset the navigation"  style={{
         display: showMe?"block":"none"
       }}>
         <div className="flex flex-col space-y-6" data-dev-hint="optional div for having an extra footer navigation">
@@ -174,7 +174,7 @@ export default function Playlists() {
               </svg>
           </button>  
 {/* 
-============================================== */}
+===================================================== */}
 
 
 <div className="flex flex-col min-h-screen">
