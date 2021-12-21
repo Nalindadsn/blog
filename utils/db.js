9b162db1b911aa01450713543842bfd5edd857cf
nalindadsn <nalindadsn@gmail.com>
2021-12-21 20:11:55 +0530

    add 38

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,8 @@
-/* This is a database connection function*/
 import mongoose from 'mongoose';
-const connection = {}; /* creating connection object*/
+
+const connection = {};
 
 async function connect() {
-  /* check if we have connection to our databse*/
   if (connection.isConnected) {
     console.log('already connected');
     return;
@@ -11,33 +10,36 @@ async function connect() {
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState;
     if (connection.isConnected === 1) {
-      console.log('use pre connection\n----------');
+      console.log('use previous connection');
       return;
     }
     await mongoose.disconnect();
-    console.log('dev disconnected\n----------');
   }
-  /* connecting to our database */
   const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
   });
-  console.log('----------\nnew connection');
+  console.log('new connection');
   connection.isConnected = db.connections[0].readyState;
 }
 
 async function disconnect() {
-  /* check if we have connection to our databse*/
   if (connection.isConnected) {
-    const testProd = false;
-    if (process.env.NODE_ENV === 'production' || testProd) {
+    if (process.env.NODE_ENV === 'production') {
       await mongoose.disconnect();
-      console.log('disconnected\n----------');
       connection.isConnected = false;
     } else {
       console.log('not disconnected');
     }
   }
 }
-export default { connect, disconnect };
+
+function convertDocToObj(doc) {
+  doc._id = doc._id.toString();
+  doc.createdAt = doc.createdAt.toString();
+  doc.updatedAt = doc.updatedAt.toString();
+  return doc;
+}
+
+const db = { connect, disconnect, convertDocToObj };
+export default db;
