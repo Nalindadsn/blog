efd5450a41ef4c41c1951f0d24316e7b3bfe9514
nalindadsn <nalindadsn@gmail.com>
2021-12-21 07:26:00 +0530

    add 34

--- pages/contact.js
+++ pages/contact.js
@@ -230,7 +230,7 @@ export default function Contact() {
         </label>
       </div>
       <button className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded" type="submit" >
-        Send Message
+        Send 
       </button>
     </div>
       
