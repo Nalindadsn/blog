7018c08c998b9f1cb00c4ba3bc6fa3c21d2357a9
nalindadsn <nalindadsn@gmail.com>
2021-12-21 23:53:09 +0530

    add 42

--- pages/quizzes.js
+++ pages/quizzes.js
@@ -249,11 +249,11 @@ export default function Quizzes() {
 
 </div>
 
-<div class="flex mb-4">
-  <div class="w-1/2 "></div>
-  <div class="w-1/2  text-right">
-  <button class="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white mr-3"> Skip</button>
-  <button class="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white"> Next</button>
+<div className="flex mb-4">
+  <div className="w-1/2 "></div>
+  <div className="w-1/2  text-right">
+  <button className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white mr-3"> Skip</button>
+  <button className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white"> Next</button>
   </div>
 </div>
 
