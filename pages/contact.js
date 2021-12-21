3e3a6a559379eac24b27272895a471bb5951a4e8
nalindadsn <nalindadsn@gmail.com>
2021-12-21 07:45:49 +0530

    add 35

--- pages/contact.js
+++ pages/contact.js
@@ -156,7 +156,38 @@ export default function Contact() {
 
 
 
+  <div className="grid grid-cols-1 md:grid-cols-12 ">
+    <div className="bg-gray-900 md:col-span-4 p-10 text-white">
+      <p className="mt-4 text-sm leading-7 font-regular uppercase">
+        Contact
+      </p>
+      <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight">
+        Get In <span className="text-indigo-600">Touch</span>
+      </h3>
+      <p className="mt-4 leading-7 text-gray-200">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </p>
+      
+      <div className="flex items-center mt-5">
 
+
+      </div>
+      <div className="flex items-center mt-5">
+
+
+
+      </div>
+      <div className="flex items-center mt-5">
+
+
+
+      </div>
+      
+    </div>
+      
+      
+    
+  </div>
 
 
 
