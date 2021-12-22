ac2f753d6e52975fbe717b9c5986119c906f7a22
nalindadsn <nalindadsn@gmail.com>
2021-12-22 12:22:01 +0530

    add 44

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
     setShowMe2(!showMe2);
   }
 
+
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -129,7 +131,7 @@ export default function Home() {
 
 <div className=" mx-auto  ">
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-    <div className="  relative  py-12  pl-12 ml-12">
+    <div className="  relative  py-12  pl-12 ml-12  hidden md:block">
     <figcaption className="absolute -left-5 top-24 text-lg -mt-16 text-white px-4 pt-12  ">
 
 
@@ -151,8 +153,28 @@ export default function Home() {
 
     </div>
     <div className=" justify-center relative  ">
+    <figcaption className="absolute right-0 top-24 text-lg -mt-16 text-white px-4 pt-12  ">
 
 
+<div className=" p-5">
+  <h1  className="bg-sky-600 px-2 ">F</h1>
+  <h1  className="bg-red-500 px-2 ">Y</h1>
+  <h1  className="bg-sky-300 px-2 ">T</h1>
+</div>
+</figcaption>
+    <figcaption className="absolute -left-5 top-0 text-lg -mt-16 text-white px-4 pt-12   block md:hidden">
+
+
+<div className="shadow p-5">
+<h4 className="font-medium text-white pt-20 text-shadow-lg	">Same chats, new different experience</h4>
+      <h2 className="font-bold text-5xl text-white leading-tight mt-3 mb-6 text-shadow-lg	">It looks better and cleaner!</h2>
+      <span className="font-small text-white pt-20 text-shadow-lg	 "> Your favorite messaging app just got the best looking update yet! Now it feels like a breath of fresh air.</span>
+      <div id="buttons" className="font-medium flex flex-row mt-6">
+        <div className="mr-3 pl-0 p-2 cursor-pointer hover:underline  text-gray-300 "><a href="#watch-video">Watch video</a></div>
+        <div className="mx-4 p-2 px-4 border-2 text-white border-white rounded-lg hover:text-white hover:bg-gray-400 cursor-pointer">Download</div>
+      </div>
+</div>
+</figcaption>
 
 
 <div className='bg-white h-12'></div>
