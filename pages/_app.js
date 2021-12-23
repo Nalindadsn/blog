1d3ca99170c95bab85ea38979333b772ed2602d4
nalindadsn <nalindadsn@gmail.com>
2021-12-23 08:40:00 +0530

    morning fix

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import '../styles/index.css'
-import { StoreProvider } from '../utils/Store'
-
-function MyApp({ Component, pageProps }) {
-  return (<StoreProvider><Component {...pageProps} /></StoreProvider>)
-}
-
-export default MyApp
