9cbd13aa34ecfed1a10bc2b985c8b9e854436bcb
nalindadsn <nalindadsn@gmail.com>
2021-12-24 14:27:42 +0530

    morning fix 4

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,31 @@
 
+import axios from 'axios';
+import React, { useState } from "react";
+
 import Layout from '../components/Layout'
 
 
-export default function T() {
-
+export default function Login() {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const submitHandler = async(e) => {
+e.preventDefault()
+try {
+const {data}=await axios.post('/api/users/login',{email,password})
+    alert('success')
+} catch (err) {
+    alert(err.message)
+   
+}
+      };
     return (
     <Layout>---------
-        <form>
-            <input type="text" name="email" id="email" className="border" /><br/>
-            <input type="password" name="password" id="password" className="border" /><br/>
+        <form onSubmit={submitHandler} >
+            <input type="text" name="email" id="email" className="border" 
+            onChange={e=>setEmail(e=e.target.value)}
+             /><br/>
+            <input type="password" name="password" id="password" className="border" 
+            onChange={e=>setPassword(e=e.target.value)} /><br/>
             <button type="submit">submit</button>
 
         </form>----------
