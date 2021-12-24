fc9647070c802bbb307fa58212837b7827b629a7
nalindadsn <nalindadsn@gmail.com>
2021-12-24 14:06:04 +0530

    morning fix 4

--- pages/login.js
+++ pages/login.js
@@ -5,9 +5,13 @@ import Layout from '../components/Layout'
 export default function T() {
 
     return (
-    <Layout>
+    <Layout>---------
+        <form>
+            <input type="text" name="email" id="email" className="border" /><br/>
+            <input type="password" name="password" id="password" className="border" /><br/>
+            <button type="submit">submit</button>
 
-jjj
+        </form>----------
     </Layout>
   )
 }
