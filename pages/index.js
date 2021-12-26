86658f19ebe372174082e6323c3de5befa2c4808
nalindadsn <nalindadsn@gmail.com>
2021-12-26 20:10:35 +0530

    userD

--- pages/index.js
+++ pages/index.js
@@ -130,7 +130,7 @@ return (
 
 
 <div className='bg-white h-12'></div>
-<img className=" pb-14 object-cover h-full w-full " src="https://res.cloudinary.com/masterdevs/image/upload/v1640121452/codeaddon/codeaddon-banner-web-development_vibee4.png" />
+<Image width="675" height="528" className=" pb-14 object-cover h-full w-full " src="https://res.cloudinary.com/masterdevs/image/upload/v1640121452/codeaddon/codeaddon-banner-web-development_vibee4.png" alt='codeaddon-banner-web-development' />
 
 
 
@@ -200,7 +200,7 @@ return (
               <div className="flex flex-wrap justify-between items-center mt-6 ">
                 <div className="inline-flex items-center shadow">
                   <div className="w-10 h-10 rounded-full overflow-hidden flex-shrink-0 ">
-                    <img src="https://res.cloudinary.com/masterdevs/image/upload/v1640114706/codeaddon/nalinda-dissanayaka_u5uh0z.jpg" />
+                    <Image width="256" height="256" src="https://res.cloudinary.com/masterdevs/image/upload/v1640114706/codeaddon/nalinda-dissanayaka_u5uh0z.jpg"alt='Nalinda Dissanayaka - author' />
                   </div>
                   <div className="flex-1 pl-2">
                     <h2 className="text-white mb-1">Nalinda Dissanayaka</h2>
