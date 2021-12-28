f4ed06875f1c6628380241666b6515122ded61b2
nalindadsn <nalindadsn@gmail.com>
2021-12-28 12:49:20 +0530

    user up 3

--- pages/login.js
+++ pages/login.js
@@ -46,7 +46,7 @@ router.push(redirect || '/');
              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password" >password</label>
             <input type="password" name="password" id="password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
             onChange={e=>setPassword(e=e.target.value)} /><br/>
-            <button type="submit">submit</button>
+            <button type="submit" className='mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>submit</button>
 
         </form>
 
