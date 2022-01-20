78924e0bc33f9f39fbdb2d592f2789a91267e10b
nalindadsn <nalindadsn@gmail.com>
2022-01-20 21:59:30 +0530

    descriptive message 3

--- pages/index.js
+++ pages/index.js
@@ -159,26 +159,34 @@ const Home = (props) => {
 
   
     <div key={product._id} className="rounded overflow-hidden shadow-lg flex flex-col">
-  <a className='p-2 bg-gray-800'>
+<Link href={`/post/${product.slug}`}>
+<a className='p-2 bg-gray-800'>
     <Image className="w-full" src={product.image} width="886" height="332" alt={product.name}/>
 </a>
+
+</Link>
   <div className="px-6 py-4 mb-auto">
     <div className="mb-3">
-      <a href="#" className="text-xs text-indigo-600 transition duration-500 ease-in-out">
-{product.category}
-</a>
+<Link href={product.category}>
+<a className="text-xs text-indigo-600 transition duration-500 ease-in-out">{product.category}</a>
+</Link>
+      
+      
 
   
 </div>
-    <a href="#" className="font-medium text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2">{product.name}</a>
+<Link href={`/post/${product.slug}`}>
+  <a className="font-medium text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2">{product.name}</a>
+</Link>
     <p className="text-gray-500 text-sm">
     {product.descriptionShort}
     </p>
   </div>
   <div className="flex items-center px-6 pb-4">
-      <a href="#"><Image width={50} height={50} className="w-12 h-12 rounded-full mr-2" src={"https://res.cloudinary.com/masterdevs/image/upload/v1640114706/codeaddon/nalinda-dissanayaka_u5uh0z.jpg"} alt="Nalinda Dissanayaka"/></a>
+      
+      <Image width={50} height={50} className="w-12 h-12 rounded-full mr-2" src={"https://res.cloudinary.com/masterdevs/image/upload/v1640114706/codeaddon/nalinda-dissanayaka_u5uh0z.jpg"} alt="Nalinda Dissanayaka"/>
       <div className="text-sm">
-        <a href="#" className="text-gray-900 font-medium leading-none hover:text-indigo-600">Nalinda Dissanayaka</a>
+        <h4 className="text-gray-900 font-medium leading-none hover:text-indigo-600">Nalinda Dissanayaka</h4>
         <p className="text-gray-600">{product.createdAt}</p>
       </div>
     </div>
