5139229d63ec8c12e897b7ec9afff45ec084651d
nalindadsn <nalindadsn@gmail.com>
2022-01-22 08:18:56 +0530

    descriptive message oop 0

--- components/NavBar.js
+++ components/NavBar.js
@@ -193,10 +193,10 @@ useEffect(() => {
 
 
 </nav>
-<div className="absolute inset-y-0 left-0 flex items-center md:hidden z-30">
+<div className="absolute inset-y-0 left-0 flex items-center md:hidden z-50">
   <button  onClick={toggle} type="button" className={" inline-flex items-center justify-center p-2  text-gray-400 hover:text-white bg-gray-600/20  hover:bg-gray-700/50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white fixed top-16 right-0 z-50 "} aria-controls="mobile-menu" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
-          
+          MENU
           
           <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
